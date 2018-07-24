a8a86bdd3841bb24f35a23a8409ab15e7a85e18c
System Administrator <root@macks-MacBook-Air.local>
2018-07-24 21:22:34 +0530

    firstcommit

---
+++ final/js/myscript.js
@@ -0,0 +1,14 @@
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function openC(evt, initials) {
+
+	document.div.style.backgroudcolor = "blue";
+ 
+}
